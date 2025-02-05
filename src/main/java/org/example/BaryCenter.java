ad5800d90b34a7d2c456f5924d0540710022779f
almdgustadev <gunna2513@gmail.com>
2025-02-04 23:50:44 -0300

    add comentarios ao codigo

--- src/main/java/org/example/BaryCenter.java
+++ src/main/java/org/example/BaryCenter.java
@@ -4,13 +4,13 @@ import java.text.DecimalFormat;
 
 public class BaryCenter {
     public static double[] barTriang(double[] x, double[] y, double[] z) {
-        double[] result = new double[2];
-        result[0] = (x[0] + y[0] + z[0]) / 3;
-        result[1] = (x[1] + y[1] + z[1]) / 3;
-        DecimalFormat df = new DecimalFormat("#.####");
+        double[] result = new double[2]; // crinado um array para armazenar as coordenadas do baricentro
+        result[0] = (x[0] + y[0] + z[0]) / 3; // armazenando o resultado de x0
+        result[1] = (x[1] + y[1] + z[1]) / 3; // armazenando o resultdo de y0
+        DecimalFormat df = new DecimalFormat("#.####");// criando um objeto DecimalFoormat para formatar as coordenadas em 4 casas decimais
 
         result[0] = Double.parseDouble(df.format(result[0]));
-        result[1] = Double.parseDouble(df.format(result[1]));
+        result[1] = Double.parseDouble(df.format(result[1])); // formatando tanto x0 quanto y0 para 4 casa decimais utilizando df.format
 
         return result;
     }
--- src/main/java/org/example/Kata.java
+++ src/main/java/org/example/Kata.java
@@ -2,11 +2,11 @@ package org.example;
 
 class Kata {
     public static String multiTable(int num) {
-        String result = "";
+        String result = ""; // criando variavel String result e inicializando com espaço vazio
         for (int i = 1 ; i <=10; i++){
             result += i +" * " + num + " = " + (num*i)+"\n";
-        }
-        return result.trim();
+        } // dentro do for , concatenamos a multiplicação na variavel result
+        return result.trim();// trim() remove a linha extra no inicio e/ou final
     }
 
     public static void main(String[] args) {
